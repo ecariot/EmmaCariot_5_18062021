2f09f06aafae70f411079df49e625c3a9f4d208b
Emma Cariot <emm.cariot@gmail.com>
2021-07-15 10:33:13 +0200

    gestion du panier

--- scripts/choice.js
+++ scripts/choice.js
@@ -8,19 +8,22 @@ const productCaseName = document.querySelector(".product-case-infos-title");
 const productCaseDescription = document.querySelector(".product-case-infos-description");
 const productCasePrice = document.querySelector(".product-case-infos-price");
 
+const addBtn = document.querySelector(".add-to-cart");
+const bearQuantity = document.querySelector("#quantity");
+
+
 if (id) {
   getArticle();
 } else {
   displayError();
 }
 
+addToCart
 
 function displayError() {
   container.remove();
   document.querySelector(".error").style.display = "block";
 }
-
-
 
 function getArticle() {
   // On récupère le produit cliqué sur l'index
@@ -54,4 +57,34 @@ function getArticle() {
         document.querySelector(".bear-color select").appendChild(option); 
       }
     })
+};
+
+addToCart();
+
+function addToCart() {
+  addBtn.addEventListener("click", () => {
+    if (bearQuantity.value > 0) {
+      const productAdded = {
+        name: productCaseName.innerHTML,
+        price: parseFloat(productCasePrice),
+        quantity: parseFloat(bearQuantity),
+        _id: id,
+      };
+    }
+  });
+
+
+//Gestion du local storage
+//Création du tableau qui contiendra nos produits 
+  const arrayProducts = [];
+
+//Si le LS est déjà rempli on y accède avec getItem
+  if (localStorage.getItem("products") !== null) {
+    arrayProducts = JSON.parse(localStorage.getItem("products"));
+    
+    
+ // Si le LS est vide, on le crée avec le produit ajouté
+  } 
+    arrayProducts.push(productAdded);
+    localStorage.setItem("products", JSON.stringify(arrayProducts));
 }
