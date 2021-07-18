79f4a1a5f810887b7ce8e8af8937923ad8943aec
Emma Cariot <emm.cariot@gmail.com>
2021-07-18 10:24:49 +0200

    css et cart ok

--- scripts/choice.js
+++ scripts/choice.js
@@ -11,14 +11,17 @@ const productCasePrice = document.querySelector(".product-case-infos-price");
 const addBtn = document.querySelector(".add-to-cart");
 const bearQuantity = document.querySelector("#quantity");
 
+let teddy = {};
+
 
 if (id) {
   getArticle();
+  addToCart();
 } else {
   displayError();
 }
 
-addToCart
+
 
 function displayError() {
   container.remove();
@@ -42,6 +45,7 @@ function getArticle() {
     })
     .then(function (article) {
       // On place les données reçues de l'API aux bons endroits sur la page
+      teddy = article;
       productCaseName.innerHTML = "Nom: " + article.name;
       productCaseImage.src = article.imageUrl;
       productCaseDescription.innerText = "Description: " + article.description;
@@ -59,32 +63,30 @@ function getArticle() {
     })
 };
 
-addToCart();
 
 function addToCart() {
   addBtn.addEventListener("click", () => {
     if (bearQuantity.value > 0) {
-      const productAdded = {
-        name: productCaseName.innerHTML,
-        price: parseFloat(productCasePrice),
-        quantity: parseFloat(bearQuantity),
-        _id: id,
-      };
+      teddy.quantity = parseInt(bearQuantity.value);
+      addToLocalStorage(teddy);    
     }
   });
-
-
-//Gestion du local storage
-//Création du tableau qui contiendra nos produits 
-  const arrayProducts = [];
-
-//Si le LS est déjà rempli on y accède avec getItem
-  if (localStorage.getItem("products") !== null) {
-    arrayProducts = JSON.parse(localStorage.getItem("products"));
-    
-    
- // Si le LS est vide, on le crée avec le produit ajouté
-  } 
-    arrayProducts.push(productAdded);
-    localStorage.setItem("products", JSON.stringify(arrayProducts));
 }
+
+function addToLocalStorage(teddy) {
+  let products = [];
+  let exist = false;
+  if (localStorage.getItem("products") !== null) {
+    products = JSON.parse(localStorage.getItem("products"));
+  }
+  for(let i = 0; i < products.length; i++){
+    if(id === products[i]._id){
+      exist = true;
+    }
+  }
+  if(exist === false){
+    products.push(teddy);
+  } else 
+  localStorage.setItem("products", JSON.stringify(products));
+}
+
